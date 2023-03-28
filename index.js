63011f1f65ad751731a609054925c46929d30b78
Luiz Fernando Ferreira <luiz_f96@hotmail.com>
2023-03-28 20:05:28 -0300

    Carregando arquivos

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
+import fs from 'fs';
 import chalk from "chalk";
 
-console.log(chalk.blue('Hello world!'));
-console.log(chalk.black.bgGreen.bold('olá mundo'));
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/');
-console.log(chalk.yellowBright.bgBlueBright('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/'));
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+// Função assíncrona com async/wait
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+// Função assíncrona com then
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto)=> console.log(chalk.green(texto)))
+//         .catch(trataErro) // (erro=> trataErro(erro))
+// }
+
+// Função síncrona
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
+//         if(erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos');
