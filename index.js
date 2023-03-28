0aa67243e4cb1c6862073fe6fa39bc3b0fd7e154
Luiz Fernando Ferreira <luiz_f96@hotmail.com>
2023-03-28 18:38:30 -0300

    Instalando dependencias

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('olá mundo');
+import chalk from "chalk";
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.black.bgGreen.bold('olá mundo'));
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/');
+console.log(chalk.yellowBright.bgBlueBright('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/'));
